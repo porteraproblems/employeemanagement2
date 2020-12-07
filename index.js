c7931d671a355e3b860f1bba990c192ae986c6a6
Alexander Portera <aportera3@gmail.com>
2020-12-06 22:49:16 -0500

    commit

--- index.js
+++ index.js
@@ -122,10 +122,11 @@ async function viewEmployeesByDepartment() {
   const departments = await db.findAllDepartments();
 
   const departmentChoices = departments.map(({ id, name }) => ({
-    // CREATE TWO PROPERTIES name AMD value FOR THIS OBJECT. THE PROPERTY name SHOULD CONTAIN THE NAME OF THE DEPARTMENT.
+    // CREATE TWO PROPERTIES name AND value FOR THIS OBJECT. THE PROPERTY name SHOULD CONTAIN THE NAME OF THE DEPARTMENT.
     // THE PROPERTY value SHOULD CONTAIN id.
     // THIS OBJECT FOR EACH MANAGER WILL RETURN TO MAP() TO CONSTRUCT AN ARRAY TO BE RETURNED AND BE STORED TO managerChoices.
-    // YOUR CODE HERE
+    name: name,
+    value: id
   }));
 
   const { departmentId } = await prompt([
@@ -152,8 +153,8 @@ async function updateEmployeeRole() {
     // CREATE TWO PROPERTIES name AMD value FOR THIS OBJECT. THE PROPERTY name SHOULD CONTAIN THE CONCATENATION OF THE FIRST HAME AND THE LAST NAME.
     // THE PROPERTY value SHOULD CONTAIN id.
     // THIS OBJECT FOR EACH MANAGER WILL RETURN TO MAP() TO CONSTRUCT AN ARRAY TO BE RETURNED AND BE STORED TO managerChoices.
-    // YOUR CODE HERE    
-
+    name: first_name + " " + last_name,
+    value: id
   }));
 
   const { employeeId } = await prompt([
@@ -286,8 +287,8 @@ async function addEmployee() {
     // CREATE TWO PROPERTIES name AMD value FOR THIS OBJECT. THE PROPERTY name SHOULD CONTAIN THE CONCATENATION OF THE FIRST HAME AND THE LAST NAME.
     // THE PROPERTY value SHOULD CONTAIN id.
     // THIS OBJECT FOR EACH MANAGER WILL RETURN TO MAP() TO CONSTRUCT AN ARRAY TO BE RETURNED AND BE STORED TO managerChoices.
-    // YOUR CODE HERE    
-
+    name: first_name + " " + last_name,
+    value: id
   }));
   managerChoices.unshift({ name: "None", value: null });
 
