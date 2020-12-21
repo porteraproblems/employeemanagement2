2d9b72a68cbdd64b34deb78ee61db7813d37fe69
Alexander Portera <aportera3@gmail.com>
2020-12-21 10:56:27 -0500

    fixed schemas

--- db/seed.sql
+++ db/seed.sql
@@ -1,13 +1,15 @@
 use employees;
 
-INSERT INTO department (dept_name)
+INSERT INTO department 
+    (dept_name)
 VALUES
     ('Sales'),
     ('Engineering'),
     ('Finance'),
     ('Legal');
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO role 
+    (title, salary, department_id)
 VALUES
     ('Sales Lead', 100000, 1),
     ('Salesperson', 80000, 1),
@@ -18,7 +20,8 @@ VALUES
     ('Legal Team Lead', 250000, 4),
     ('Lawyer', 190000, 4);
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employee 
+    (first_name, last_name, role_id, manager_id)
 VALUES
     ('John', 'Doe', 1, NULL),
     ('Mike', 'Chan', 2, 1),
